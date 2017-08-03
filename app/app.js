ffe231f393ef78f83d2b0fad434381c605ce6795
davidtom <davidjtomczyk@gmail.com>
2017-08-03 19:13:55 -0400

    add comment for future reference

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   // Actions that happen on page load for all users joining page
   SessionController.syncGame()
-  CategoryAdapter.index()
+  CategoryAdapter.index() // TODO: remove this - this will be handled by GameAdapter.current()
   .then(function(){
     // create controllers
     gameController = new GameController();
